8843b4962792beeb6925ab518409e612961082fb
Anitaaaaaaaaaaaaaaaa <160527212+Anitaaaaaaaaaaaaaaaa@users.noreply.github.com>
2025-07-31 18:30:12 -0300

    Add files via upload

---
+++ superclasse MCR.java
@@ -0,0 +1,17 @@
+package Heranca;
+
+public class MyChemicalRomance {
+     
+  Int ano_formacao ;
+  String membros;
+  Int numero_albums;
+
+  public MyChemicalRomance(){}
+
+  public MyChemicalRomance (Int ano_formacao, String membros, Int numero_albums){
+   this.ano_formacao = 2001;
+   this.membros = GerardWay, MikeyWay, FrankIero, RayToro;
+   this.numero_albums = 4;
+  }
+} 
+
