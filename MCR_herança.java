6273fc44ffe7c0c298f7503a44a8333dda5816bc
Anitaaaaaaaaaaaaaaaa <160527212+Anitaaaaaaaaaaaaaaaa@users.noreply.github.com>
2025-07-31 18:29:45 -0300

    Add files via upload

---
+++ MCR_herança.java
@@ -0,0 +1,40 @@
+package Heranca;
+
+public class MyChemicalRomance {
+     
+  Int ano_formacao ;
+  String membros;
+  Int numero_albums;
+
+  public Gorillaz(){}
+
+  public Gorillaz (Int ano_formacao, String membros, Int numero_albums){
+   this.ano_formacao = 2001;
+   this.membros = GerardWay, MikeyWay, FrankIero, RayToro;
+   this.numero_albums = 4;
+  }
+}
+
+
+package Heranca;
+public class Gorillaz_album extends Gorillaz {
+
+  String musica1;
+  String musica2;
+  String musica3;
+
+  public MyChemicalRomance_album() {
+    
+ super();
+}
+
+  public MyChemicalRomance_album(Int ano_formacao, String membros,  Int numero_albums, 
+  String musica1, String musica2,
+   String musica3) {
+
+    super(ano_formacao, membros, numero_albums);
+  
+    this.musica1 = Re-Hash;
+    this.musica2 = Tomorrow_Comes_Today;
+    this.musica3 = Clint_Eastwood;
+}
